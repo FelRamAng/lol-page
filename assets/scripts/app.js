806c1165553594d7b3e756e841b0011cd907a070
JosephGlz <joseph220199@gmail.com>
2022-07-21 16:23:48 -0500

    API consume create

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,50 @@
-document.addEventListener('contextmenu', event => event.preventDefault());
+let language = 'es_ES';
+
+
+fetch(`http://ddragon.leagueoflegends.com/cdn/11.19.1/data/${language}/champion.json`)
+    .then(response => response.json())
+    .then(data => {
+        let champions = data.data;
+        let championsArray = Object.keys(champions).map(key => champions[key]);
+        let championsList = document.querySelector('.container');
+        championsArray.forEach((champion, id) => {
+            let championElement = document.createElement('div');
+            championElement.classList.add('champion');
+            championElement.innerHTML = `
+                <img src="http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg" alt="${champion.id}" width="120px">
+                <h3>${champion.name}</h3>
+                <p>${champion.title}</p>
+            `;
+            championsList.appendChild(championElement);
+            console.log(champion);
+        });
+    }
+    ).catch(error => console.log(error));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
