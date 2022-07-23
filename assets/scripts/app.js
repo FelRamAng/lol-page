047c8b779c700a78a23d0cc8e2baa73eb1178083
JosephGlz <joseph220199@gmail.com>
2022-07-23 00:48:04 -0500

    new style inner

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,22 @@
 let language = 'es_ES';
 
 
-fetch(`http://ddragon.leagueoflegends.com/cdn/11.19.1/data/${language}/champion.json`)
-    .then(response => response.json())
-    .then(data => {
-        let champions = data.data;
-        let championsArray = Object.keys(champions).map(key => champions[key]);
-        let championsList = document.querySelector('.champion-container');
-        championsArray.forEach((champion, id) => {
+/*
+<div class="champion-card">
+            <img class="champ-img" src="http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg">
+            <div class="champ-overlap">
+                <div class="champ-info">
+                    <h3 class="champ-name">
+                        Aatrox
+                    </h3>
+                <p class="champ-title">
+                    La Espada de los Oscuros
+                </p>
+                </div>
+            </div>
+        </div>
+
+        (champion, id) => {
             let championElement = document.createElement('div');
             championElement.classList.add('champion');
             championElement.innerHTML = `
@@ -18,6 +27,37 @@ fetch(`http://ddragon.leagueoflegends.com/cdn/11.19.1/data/${language}/champion.
             championsList.appendChild(championElement);
             console.log(champion);
         });
+*/
+fetch(`http://ddragon.leagueoflegends.com/cdn/11.19.1/data/${language}/champion.json`)
+    .then(response => response.json())
+    .then(data => {
+        let champions = data.data;
+        let championsArray = Object.keys(champions).map(key => champions[key]);
+        let championsList = document.querySelector('.champ-container');
+        championsArray.forEach(champion => {
+            let championCard = document.createElement('div');
+            championCard.classList.add('champion-card');
+            let championImg = document.createElement('img');
+            championImg.classList.add('champ-img');
+            championImg.src = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`;
+            let championOverlap = document.createElement('div');
+            championOverlap.classList.add('champ-overlap');
+            let championInfo = document.createElement('div');
+            championInfo.classList.add('champ-info');
+            let championName = document.createElement('h3');
+            championName.classList.add('champ-name');
+            championName.innerHTML = champion.id;
+            let championTitle = document.createElement('p');
+            championTitle.classList.add('champ-title');
+            championTitle.innerHTML = champion.title;
+            championInfo.appendChild(championName);
+            championInfo.appendChild(championTitle);
+            championOverlap.appendChild(championInfo);
+            championCard.appendChild(championImg);
+            championCard.appendChild(championOverlap);
+            championsList.appendChild(championCard);
+        }
+        );
     }
     ).catch(error => console.log(error));
 
